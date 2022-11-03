5caf36bce9df5b9c0cdb5595235dc4798a12e433
Aditya Srivastava <117251164+AdiSrivastava11@users.noreply.github.com>
2022-11-03 11:55:12 +0530

    Update Account.java

--- collection/Account.java
+++ collection/Account.java
@@ -1,1 +1,51 @@
+package collection;
 
+public class Account implements Comparable<Account>{
+
+	private String accountname;
+	private int acctnum;
+	private double balance;
+
+	public Account(String accountname, double balance,int acctnum) {
+		super();
+		this.accountname = accountname;
+		this.acctnum = acctnum;
+		this.balance = balance;
+	}
+
+	public String getAccountname() {
+		return accountname;
+	}
+
+	public void setAccountname(String accountname) {
+		this.accountname = accountname;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public int getAcctNo() {
+		return acctnum;
+	}
+
+	public void deposit(double amt) {
+		this.balance = this.balance + amt;
+	}
+
+	@Override
+	    public String toString() {
+	        return "[accname=" + accountname + ", acctnum=" + acctnum + ", balance=" + balance + "]";
+	}
+	
+	@Override
+	public int compareTo(Account a)
+	{
+		return this.acctnum - a.acctnum;
+	}
+
+}
