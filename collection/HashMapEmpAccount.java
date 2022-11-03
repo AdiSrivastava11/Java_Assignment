5b6c20ef94d56d5a912d6fdc6763ae3801f9278e
Aditya Srivastava <117251164+AdiSrivastava11@users.noreply.github.com>
2022-11-03 12:02:44 +0530

    Update HashMapEmpAccount.java

--- collection/HashMapEmpAccount.java
+++ collection/HashMapEmpAccount.java
@@ -1,1 +1,22 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEmpAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Employee,Double> smp = new HashMap<Employee,Double>();
+		Employee dr1 = new Employee(2,"Seb",25000,10);
+		
+		smp.put(dr1, 25000.0);
+		smp.put(new Employee(1,"Sainz",15000,10), 15000.0);
+		smp.put(new Employee(3,"Perez",35000,20), 30000.0);
+		System.out.println(smp);
+		System.out.println("Abhinav exist in map:"+smp.containsKey(dr1));
+		System.out.println("Account with balance amount 30000 exist:" + smp.containsValue(30000.0));
+	}
+
+}
 
