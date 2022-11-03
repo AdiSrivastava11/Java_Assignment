24f5e2c4829e5197c280f080b31013ba216c213b
Aditya Srivastava <117251164+AdiSrivastava11@users.noreply.github.com>
2022-11-03 12:01:07 +0530

    Update ComparatorEmplDeptnum.java

--- collection/ComparatorEmplDeptnum.java
+++ collection/ComparatorEmplDeptnum.java
@@ -1,1 +1,15 @@
+package collection;
+
+import java.util.Comparator;
+
+public class ComparatorEmplDeptnum implements Comparator<Employee>{
+
+    public int compare(Employee e1,Employee e2)
+    {
+        System.out.println("comparing e1.deptno:"+e1.getDeptno()+" and e2.deptno:"+e2.getDeptno());
+        return(int)(e1.getDeptno() + e2.getDeptno());
+    
+
+}
+}
 
